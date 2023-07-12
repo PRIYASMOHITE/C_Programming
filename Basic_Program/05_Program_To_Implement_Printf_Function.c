3f1dbff78103ee7db1d723894576bfca61812a57
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-12 21:48:35 +0530

    C_Program

---
+++ Basic_Program/05_Program_To_Implement_Printf_Function.c
@@ -0,0 +1,17 @@
+// Program To Implement printf Function
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int num = 0;
+   float Dist = 0.0;
+
+       printf("\n Welcome To \"Talbid\"!!!");
+
+       printf ("\n Value of Number = %d. \t Distance = %0.2f.\a", num , Dist);
+       getch();
+       return 0;
+}
+
