a740c14b8f3147ce59b776184651d7fe0c424946
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-14 22:39:37 +0530

    C_Program

---
+++ Basic_Program/11_Write_A_Program_To_Do_While_Demonstration.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int cnt = 1;
+    do
+
+    {
+         printf("\n Welcome");
+         printf("\n Hello");
+         cnt++;
+
+    }
+    while(cnt <= 3);
+
+    printf("\n\n Thanks!!!");
+    getch();
+    return 0;
+}
+
