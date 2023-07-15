0bf4db82e6b8003e7a3347e79948a8387bbe59e7
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-15 09:00:06 -0700

    C_Program

---
+++ Array_Program/04_Create_Array_Of_7_Element_With_Null_(0)_Initilzation_&_Accept_All_Element_Without_Loop_Display_All_Element.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Value [7]={11,12,13,14,15,16,17};
+    printf("\n First Element  = %d",Value[0]);
+    printf("\n Second Element = %d",Value[1]);
+    printf("\n Third Element  = %d",Value[2]);
+    printf("\n Fourth Element = %d",Value[3]);
+    printf("\n Fifth Element  = %d",Value[4]);
+    printf("\n Sixth Element  = %d",Value[5]);
+    printf("\n Seventh Element= %d",Value[6]);
+    getch();
+    return 0;
+}
