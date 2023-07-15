e06b3149295d7e75dd3e39dd662e12c777cf3273
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-15 20:02:08 +0530

    C_Program

---
+++ Array_Program/02_Create_Array_With_Intilization_&_Display_All_Element.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Value [5]= {10,20,30,40,50};
+
+    printf("\n first Element  = %d",Value [0]);
+    printf("\n second Element = %d",Value [1]);
+    printf("\n Third Element  = %d",Value [2]);
+    printf("\n fourth Element = %d",Value [3]);
+    printf("\n fifth Element  = %d",Value [4]);
+    getch();
+    return 0;
+}
+
+
+
+
