374f40a1252129afdd144dfb50490663fad1e487
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-16 10:19:47 -0700

    C_Program

---
+++ Pattern_Printing/12_Write_A_Program_For_New_Pattern_Printing.c
@@ -0,0 +1,19 @@
+#include<Stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" 7 ");
+
+        }
+        printf("\n");
+    }
+    return 0;
+    getch();
+}
+
