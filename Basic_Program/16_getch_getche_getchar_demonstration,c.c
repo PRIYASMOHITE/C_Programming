4048d4e9e6f6aa3ec6cf30dff17136cf04f236d6
PRIYASMOHITE <136099117+PRIYASMOHITE@users.noreply.github.com>
2023-07-19 10:17:03 -0700

    C_Program

---
+++ Basic_Program/16_getch_getche_getchar_demonstration,c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch = 'A';
+    printf("\n Enter a letter = ");
+    ch = getche();
+    printf("\n Welcome To Talbid!!!\n");
+
+    printf("\n Value of given Character = %c",ch);
+
+    getche();
+    getch();
+    return 0;
+}
